84a037d5bf10596d7f2259b34fe097cf53fb648d
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-12-22 22:10:15 +0100

    responsive layout dashboard templated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 const TogglePassword = (passwordId) => {
     // select password input
     const password = document.getElementById(passwordId);
-    password.type = (password.type === "password") ? "text" : "password";
-}
+    password.type = password.type === "password" ? "text" : "password";
+};
+
+const ToggleMenu = () => {
+    const sidebar = document.getElementById("sidebarLayout");
+    if (sidebar.classList.contains("d-none")) {
+        // opens sidebar
+        sidebar.classList.remove("d-none");
+        sidebar.classList.add("d-block");
+    } else {
+        // closes sidebar
+        sidebar.classList.remove("d-block");
+        sidebar.classList.add("d-none");
+    }
+};
